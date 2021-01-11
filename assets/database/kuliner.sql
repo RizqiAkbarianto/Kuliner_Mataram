2c41423bdb6537b991f9c6abd98c8af5b827e866
RizqiAkbarianto <riskiak32@gmail.com>
2021-01-11 17:50:55 +0800

    database

--- assets/database/kuliner.sql
+++ assets/database/kuliner.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Des 2020 pada 12.59
--- Versi server: 10.1.31-MariaDB
--- Versi PHP: 7.2.3
+-- Generation Time: Jan 11, 2021 at 10:48 AM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `gambar`
+-- Table structure for table `gambar`
 --
 
 CREATE TABLE `gambar` (
@@ -37,17 +37,18 @@ CREATE TABLE `gambar` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `gambar`
+-- Dumping data for table `gambar`
 --
 
 INSERT INTO `gambar` (`id_gambar`, `id_produk`, `judul_gambar`, `gambar`, `tanggal_update`) VALUES
-(2, 3, 'Sosis Bakar 2', '5.png', '2020-11-23 15:17:26'),
-(3, 7, 'Sate - Taichan x Nyotnyot 2', 'Sata_taichan_2.jpg', '2020-12-12 11:45:24');
+(3, 7, 'Sate - Taichan x Nyotnyot 2', 'Sata_taichan_2.jpg', '2020-12-12 11:45:24'),
+(4, 10, 'Kebab2 - Babarafi', 'babarafi.jpg', '2020-12-21 17:51:55'),
+(5, 10, 'Kebab3 - Babarafi', 'babarafi2.jpg', '2020-12-21 17:54:03');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `header_transaksi`
+-- Table structure for table `header_transaksi`
 --
 
 CREATE TABLE `header_transaksi` (
@@ -68,7 +69,7 @@ CREATE TABLE `header_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kategori`
+-- Table structure for table `kategori`
 --
 
 CREATE TABLE `kategori` (
@@ -80,19 +81,20 @@ CREATE TABLE `kategori` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kategori`
+-- Dumping data for table `kategori`
 --
 
 INSERT INTO `kategori` (`id_kategori`, `slug_kategori`, `nama_kategori`, `urutan`, `tanggal_update`) VALUES
 (1, 'minuman', 'Minuman', 1, '2020-10-25 13:52:57'),
 (2, 'cake', 'Cake', 2, '2020-10-25 13:52:57'),
 (3, 'street-food', 'Street food', 3, '2020-10-25 13:46:52'),
-(4, 'makanan', 'Makanan', 4, '2020-12-12 12:13:51');
+(4, 'makanan', 'Makanan', 4, '2020-12-12 12:13:51'),
+(5, 'cemilan', 'Cemilan', 5, '2020-12-12 12:33:30');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `konfigurasi`
+-- Table structure for table `konfigurasi`
 --
 
 CREATE TABLE `konfigurasi` (
@@ -110,21 +112,23 @@ CREATE TABLE `konfigurasi` (
   `deskripsi` text,
   `logo` varchar(255) DEFAULT NULL,
   `icon` varchar(255) DEFAULT NULL,
+  `slider` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
   `rekening_pembayaran` varchar(255) DEFAULT NULL,
   `tanggal_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `konfigurasi`
+-- Dumping data for table `konfigurasi`
 --
 
-INSERT INTO `konfigurasi` (`id_konfigurasi`, `namaweb`, `tagline`, `email`, `website`, `keywords`, `metatext`, `telepon`, `alamat`, `facebook`, `instagram`, `deskripsi`, `logo`, `icon`, `rekening_pembayaran`, `tanggal_update`) VALUES
-(1, 'Kuliner.Mataram', 'Eat Well, Life Well in Lombok', 'kuliner.mataram@gmail.com', 'http://kulinermataram.com', 'Kuliner.Mataram', 'Kuliner.Mataram', '081902987365', 'Jl. Towuti Raya No.9 Perumnas Tanjung Karang', 'http://facebook.com/kuliner.mataram', 'http://instagram.com/kuliner.mataram', 'Kuliner', 'Picture4.png', 'iconlogonkl.png', '36574365348756047', '2020-12-12 12:23:37');
+INSERT INTO `konfigurasi` (`id_konfigurasi`, `namaweb`, `tagline`, `email`, `website`, `keywords`, `metatext`, `telepon`, `alamat`, `facebook`, `instagram`, `deskripsi`, `logo`, `icon`, `slider`, `login`, `rekening_pembayaran`, `tanggal_update`) VALUES
+(1, 'Kuliner.Mataram', 'Eat Well, Life Well in Lombok', 'kuliner.mataram@gmail.com', 'http://kulinermataram.com', 'Kuliner.Mataram', 'Kuliner.Mataram', '081902987365', 'Jl. Towuti Raya No.9 Perumnas Tanjung Karang', 'http://facebook.com/kuliner.mataram', 'http://instagram.com/kuliner.mataram', 'Kuliner', 'Picture4.png', 'iconlogonkl.png', 'slider1.jpg', '', '36574365348756047', '2021-01-10 04:55:27');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pelanggan`
+-- Table structure for table `pelanggan`
 --
 
 CREATE TABLE `pelanggan` (
@@ -143,7 +147,7 @@ CREATE TABLE `pelanggan` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `produk`
+-- Table structure for table `produk`
 --
 
 CREATE TABLE `produk` (
@@ -153,6 +157,10 @@ CREATE TABLE `produk` (
   `kode_produk` varchar(20) NOT NULL,
   `nama_produk` varchar(255) NOT NULL,
   `slug_produk` varchar(255) NOT NULL,
+  `instagram` varchar(255) NOT NULL,
+  `link_instagram` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `maps` varchar(1000) NOT NULL,
   `keterangan` text NOT NULL,
   `keywords` text,
   `harga` int(11) NOT NULL,
@@ -166,23 +174,23 @@ CREATE TABLE `produk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `produk`
+-- Dumping data for table `produk`
 --
 
-INSERT INTO `produk` (`id_produk`, `id_user`, `id_kategori`, `kode_produk`, `nama_produk`, `slug_produk`, `keterangan`, `keywords`, `harga`, `stok`, `gambar`, `berat`, `ukuran`, `status_produk`, `tanggal_post`, `tanggal_update`) VALUES
-(3, 1, 1, 'M1Kulo', 'Kulo', 'kulo-m1kulo', '<p>Kulo</p>\r\n', 'Kulo', 12000, 1000, 'Kulo.jpg', 1, '20X10 Cm', 'Publish', '2020-11-14 15:16:01', '2020-12-12 11:47:09'),
-(4, 1, 2, 'C1MandeCakery', 'Kue Bolu - Mande Cakery', 'kue-bolu-mande-cakery-c1mandecakery', '<p>Kue Bolu - Mande Cakery</p>\r\n', 'Kue Bolu - Mande Cakery', 20000, 100, 'Mandre_Cakery.jpg', 2, '70X70 Cm', 'Publish', '2020-12-12 13:17:32', '2020-12-12 11:47:09'),
-(5, 1, 1, 'M2Boba - Xiboba', 'Boba - Xiboba', 'boba-xiboba-m2boba-xiboba', '<p>Boba - Xiboba</p>\r\n', 'Boba - Xiboba', 15000, 100, 'Xiboba.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:22:13', '2020-12-12 11:47:35'),
-(6, 1, 2, 'C2Korean Garlic - Ni', 'Korean Garlic - Niat Baik Coffe', 'korean-garlic-niat-baik-coffe-c2korean-garlic-niat-baik-coffe', '<p>Korean Garlic - Niat Baik Coffe</p>\r\n', 'Korean Garlic - Niat Baik Coffe', 35000, 90, 'Niat_baik_Coffe.jpg', 1, '70X70 Cm', 'Publish', '2020-12-12 13:34:02', '2020-12-12 11:47:09'),
-(7, 1, 4, 'MK1Sate - Taichan x ', 'Sate - Taichan x Nyotnyot', 'sate-taichan-x-nyotnyot-mk1sate-taichan-x-n', '<p>Sate - Taichan x Nyotnyot</p>\r\n', ' Sate - Taichan x Nyotnyot', 21000, 78, 'taichan_nyot2.jpg', 1, '30X10 Cm', 'Publish', '2020-12-12 13:18:22', '2020-12-12 12:28:37'),
-(8, 1, 1, 'M3Boba - KingBoba', 'Boba - KingBoba', 'boba-kingboba-m3boba-kingboba', '<p>Boba - KingBoba</p>\r\n', 'Boba - KingBoba', 15000, 89, 'king_boba.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:22:21', '2020-12-12 12:22:21'),
-(9, 1, 1, 'M4jus - Nyotnyot', 'Jus - Nyotnyot', 'jus-nyotnyot-m4jus-nyotnyot', '<p>jus - Nyotnyot</p>\r\n', ' jus - Nyotnyot', 15000, 86, 'nyot_nyot_.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:27:58', '2020-12-12 12:28:49'),
-(10, 1, 3, 'S2Kebab - Babarafi', 'Kebab - Babarafi', 'kebab-babarafi-s2kebab-babarafi', '<p>Kebab - Babarafi</p>\r\n', 'Kebab - Babarafi', 20000, 79, 'kebab_babarafi.jpg', 1, '70X10 Cm', 'Publish', '2020-12-12 13:22:00', '2020-12-12 12:22:00');
+INSERT INTO `produk` (`id_produk`, `id_user`, `id_kategori`, `kode_produk`, `nama_produk`, `slug_produk`, `instagram`, `link_instagram`, `alamat`, `maps`, `keterangan`, `keywords`, `harga`, `stok`, `gambar`, `berat`, `ukuran`, `status_produk`, `tanggal_post`, `tanggal_update`) VALUES
+(3, 1, 1, 'M1Kulo', 'Kulo', 'kulo-m1kulo', '', '', '', '', '<p>Kulo</p>\r\n', 'Kulo', 12000, 1000, 'Kulo.jpg', 1, '20X10 Cm', 'Publish', '2020-11-14 15:16:01', '2020-12-12 11:47:09'),
+(4, 1, 2, 'C1MandeCakery', 'Kue Bolu - Mande Cakery', 'kue-bolu-mande-cakery-c1mandecakery', '', '', '', '', '<p>Kue Bolu - Mande Cakery</p>\r\n', 'Kue Bolu - Mande Cakery', 20000, 100, 'Mandre_Cakery.jpg', 2, '70X70 Cm', 'Publish', '2020-12-12 13:17:32', '2020-12-12 11:47:09'),
+(5, 1, 1, 'M2Boba - Xiboba', 'Boba - Xiboba', 'boba-xiboba-m2boba-xiboba', '', '', '', '', '<p>Boba - Xiboba</p>\r\n', 'Boba - Xiboba', 15000, 100, 'Xiboba.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:22:13', '2020-12-12 11:47:35'),
+(6, 1, 2, 'C2Korean Garlic - Ni', 'Korean Garlic - Niat Baik Coffe', 'korean-garlic-niat-baik-coffe-c2korean-garlic-niat-baik-coffe', '', '', '', '', '<p>Korean Garlic - Niat Baik Coffe</p>\r\n', 'Korean Garlic - Niat Baik Coffe', 35000, 90, 'Niat_baik_Coffe.jpg', 1, '70X70 Cm', 'Publish', '2020-12-12 13:34:02', '2020-12-12 11:47:09'),
+(7, 1, 4, 'MK1Sate - Taichan x ', 'Sate - Taichan x Nyotnyot', 'sate-taichan-x-nyotnyot-mk1sate-taichan-x-n', '', '', '', '', '<p>Sate - Taichan x Nyotnyot</p>\r\n', ' Sate - Taichan x Nyotnyot', 21000, 78, 'taichan_nyot2.jpg', 1, '30X10 Cm', 'Publish', '2020-12-12 13:18:22', '2020-12-12 12:28:37'),
+(8, 1, 1, 'M3Boba - KingBoba', 'Boba - KingBoba', 'boba-kingboba-m3boba-kingboba', '', '', '', '', '<p>Boba - KingBoba</p>\r\n', 'Boba - KingBoba', 15000, 89, 'king_boba.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:22:21', '2020-12-12 12:22:21'),
+(9, 1, 1, 'M4jus - Nyotnyot', 'Jus - Nyotnyot', 'jus-nyotnyot-m4jus-nyotnyot', '', '', '', '', '<p>jus - Nyotnyot</p>\r\n', ' jus - Nyotnyot', 15000, 86, 'nyot_nyot_.jpg', 1, '20X10 Cm', 'Publish', '2020-12-12 13:27:58', '2020-12-12 12:28:49'),
+(10, 1, 3, 'S2Kebab - Babarafi', 'Kebab - Babarafi', 'kebab-babarafi-s2kebab-babarafi', 'babarafi.mataram', 'http://instagram.com/babarafi.mataram', 'JL. Sriwijaya, Cilinaya, Kec. cakranegara, Kota Mataram, Nusa tenggara Barat', '<iframe src=\"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3945.035973434713!2d116.12378061541615!3d-8.592539739612414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdbfdd8708739b%3A0x9b8c738e2ae74c5b!2sKebab%20Turki%20Baba%20Rafi%20Mataram!5e0!3m2!1sid!2sid!4v1610278104861!5m2!1sid!2sid\" width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0;\" allowfullscreen=\"\" aria-hidden=\"false\" tabindex=\"0\"></iframe>', '<p>Hallo kuliner lover! Sore ini mimin mampir ke @babarafi.mataram&nbsp;<br />\r\nNaah, buat kalian pecinta kebab, roti canai dan burger, di @babarafi.mataram yang menurut mimin recommended banget buat kalian cobain malam minggu ini loh, Kalo menurut mimin di @babarafi.mataram ini roti tortilla (roti kebab) nya enak banget, dengan isian sayur yang fresh fresh banget dan potongan daging di dalemnya banyak. Dengan harga 20ribuan aja, kalian udah bisa dapetin kebab yang enak dan di jamin puas deh makannya&nbsp;Untuk roti canainya di @babarafi.mataram ini kalian di suguhi topping yang melimpah seperti ini. Burgernya?? jangan di tanya lagi, mantep banget, Makanya nih mimin buru buru infoin kalian hari ini buat mampir ke @babarafi.mataram karna momentnya pas banget nih buat yang lagi jalan jalan malam mingguan bisa mampir ke @babarafi.mataram yang ada di jalan stiwijaya atau pas di teras Alfamart deket Hotel Golden Palace&nbsp;</p>\r\n', '         Kebab - Babarafi', 20000, 79, 'kebab_babarafi.jpg', 1, '70X10 Cm', 'Publish', '2020-12-12 13:22:00', '2021-01-11 06:23:52');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -201,7 +209,7 @@ CREATE TABLE `transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -215,119 +223,120 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`, `akses_level`, `tanggal_update`) VALUES
-(1, 'Rizqi Akbarianto', 'riskiak@gmail.com', 'RizqiAkbarianto', '7c222fb2927d828af22f592134e8932480637c0d', 'Admin', '2020-10-24 06:57:27');
+(1, 'Rizqi Akbarianto', 'riskiak@gmail.com', 'RizqiAkbarianto', '7c222fb2927d828af22f592134e8932480637c0d', 'Admin', '2020-10-24 06:57:27'),
+(2, 'administrator', 'admin@gmail.com', 'administrator', '7c222fb2927d828af22f592134e8932480637c0d', 'Admin', '2021-01-05 06:44:07');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `gambar`
+-- Indexes for table `gambar`
 --
 ALTER TABLE `gambar`
   ADD PRIMARY KEY (`id_gambar`);
 
 --
--- Indeks untuk tabel `header_transaksi`
+-- Indexes for table `header_transaksi`
 --
 ALTER TABLE `header_transaksi`
   ADD PRIMARY KEY (`id_header_transaksi`),
   ADD UNIQUE KEY `kode_transaksi` (`kode_transaksi`);
 
 --
--- Indeks untuk tabel `kategori`
+-- Indexes for table `kategori`
 --
 ALTER TABLE `kategori`
   ADD PRIMARY KEY (`id_kategori`);
 
 --
--- Indeks untuk tabel `konfigurasi`
+-- Indexes for table `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   ADD PRIMARY KEY (`id_konfigurasi`);
 
 --
--- Indeks untuk tabel `pelanggan`
+-- Indexes for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   ADD PRIMARY KEY (`id_pelanggan`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indeks untuk tabel `produk`
+-- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`),
   ADD UNIQUE KEY `kode_produk` (`kode_produk`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `gambar`
+-- AUTO_INCREMENT for table `gambar`
 --
 ALTER TABLE `gambar`
-  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_gambar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `header_transaksi`
+-- AUTO_INCREMENT for table `header_transaksi`
 --
 ALTER TABLE `header_transaksi`
   MODIFY `id_header_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `kategori`
+-- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `konfigurasi`
+-- AUTO_INCREMENT for table `konfigurasi`
 --
 ALTER TABLE `konfigurasi`
   MODIFY `id_konfigurasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT untuk tabel `pelanggan`
+-- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
   MODIFY `id_pelanggan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `produk`
+-- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
   MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
